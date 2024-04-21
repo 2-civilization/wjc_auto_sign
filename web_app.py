74378d75a49709c064e608017bd255421f3f44c7
ssss134 <1340554713@qq.com>
2024-04-21 22:18:55 +0800

    feat(web_app)：解决数据库locked问题 - 增加对时间段判断的操作。在签到启动时间段内禁止进行注册或获取用户信息等操作，防止引发数据库locked异常

--- web_app.py
+++ web_app.py
@@ -1,12 +1,11 @@
 from sanic import Sanic
 from sanic.response import html,json,file
-import aiofiles
 import os
 from pathlib import Path
 from db_control import DBControl
-from setting import DB_PATH,REMOTE_API_TOKEN
+from setting import DB_PATH,REMOTE_API_TOKEN,TIME_SET
 from mail_control import user_mail,reg_mail_gen
-
+from datetime import datetime,time
 
 NOW_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -14,6 +13,22 @@ DB = DBControl(DB_PATH)
 
 app = Sanic("webSubmitter")
 # app.static("/template","D:\\REPOSITORY\\PROJECTS\\Project\\WJC_Sign\\WebSubmitter\\template")
+
+async def is_db_locked() -> bool:
+    # 获取当前时间
+    now = datetime.now()
+    current_time = now.time()
+
+    # 将时间区间转换为datetime.time对象
+    start_time = time(hour=int(TIME_SET['start'].split(':')[0]), minute=int(TIME_SET['start'].split(':')[1]))
+    end_time = time(hour=int(TIME_SET['end'].split(':')[0]), minute=int(TIME_SET['end'].split(':')[1]))
+
+    if start_time <= current_time <= end_time:
+        # 处于签到时间点，禁止数据库操作
+        return True
+    else:
+        return False
+    
 
 @app.get('/')
 async def index(request):
@@ -41,6 +56,9 @@ async def getLocal(request):
 
 @app.post('/submit')
 async def submit(request):
+    if await is_db_locked():
+        return json({"code":'fail','msg':f'当前时间段（{TIME_SET["start"]} - {TIME_SET["end"]}）无法注册，请在非此时间段再重试'})
+    
     form = request.get_form()
     if user_mail('注册成功',reg_mail_gen({'account':form["username"][0],'email':form["email"][0],'coordinate':form["coordinates"][0]}),form["email"][0]):
         await DB.add_user(form["username"][0],form["password"][0],form["email"][0],form["coordinates"][0])
@@ -54,6 +72,10 @@ async def reg_success_page(request):
 
 @app.post('/getUsers')
 async def get_users_info(request):
+    if await is_db_locked():
+        return json({"code":'fail','msg':f'当前时间段（{TIME_SET["start"]} - {TIME_SET["end"]}）无法获取用户信息，请在非此时间段再重试'})
+    
+
     form = request.get_form()
     if form["token"][0] == REMOTE_API_TOKEN:
         users = await DB.get_users_info()
