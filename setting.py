1e9d8a3cc705bd31aca8f1da83df4a0dbb1754d6
ssss134 <1340554713@qq.com>
2024-04-27 20:57:35 +0800

    feat(setting)：封禁功能配置更新： - 对表结构作出调整，适应新更能 - 增加新参数FAIL_MAX_TRY_DAYS，用于设置最大连续失败天数，达到该天数将会禁用用户

--- setting.py
+++ setting.py
@@ -2,7 +2,6 @@ import logging
 import os.path as os_path
 from os import mkdir as os_mkdir
 from datetime import datetime
-import sqlite3
 
 CURRENT_PATH = os_path.dirname(os_path.abspath(__file__))
 
@@ -33,17 +32,20 @@ TIME_CHCECK_WAIT = 180  # 3分钟
 TIME_SLEEP_WAIT = 60*60*22 # 22小时
 
 SIGN_MAX_TRY_TIMES = 3 # 签到失败最多尝试次数
+FAIL_MAX_TRY_DAYS = 3   # 最大连续失败签到天数，达到该天数后将会禁用用户自动签到
 
 DB_INIT_SQL = '''
     CREATE TABLE IF NOT EXISTS users (
-        id INTEGER NOT NULL PRIMARY KEY,
+        id INTEGER NOT NULL PRIMARY KEY UNIQUE,
         pswd TEXT NOT NULL,
         email TEXT NOT NULL,
         coordinate TEXT NOT NULL,
         updateTime TEXT NOT NULL,
         signTime TEXT,
         success INTEGER DEFAULT 0,
-        total INTEGER DEFAULT 0
+        total INTEGER DEFAULT 0,
+        active INTEGER DEFAULT 1,
+        failDays INTEGER DEFAULT 0
     );
 '''
 
